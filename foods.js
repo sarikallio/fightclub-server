476e504da456809fdbb7c5ec937d3af8f9121659
sarikallio <sari.kallio.sk@gmail.com>
2019-03-18 15:43:09 +0200

    toinen testi

--- foods.js
+++ foods.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool;
 const conopts = {
-    user: 'postgres',
-    password: 'Sovelto1',
-    host: 'localhost',
-    database: 'fightclub'
+    // user: 'postgres',
+    // password: 'Sovelto1',
+    // host: 'localhost',
+    database: 'https://data.heroku.com/datastores/2d6a4007-af49-4dc5-a93d-b90b5cffeefa'
 }
 
 const pool = new Pool(conopts);
