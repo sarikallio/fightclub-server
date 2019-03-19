aae2dcc0ee0cadccb9c2b1a34ff4a657d74d73b1
sarikallio <sari.kallio.sk@gmail.com>
2019-03-19 15:38:56 +0200

    env testi

--- foods.js
+++ foods.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const Pool = require('pg').Pool;
 const conopts = {
-    user: 'hatwlzbfjizmyp',
-    password: '18d81962334ae5762081ca9410cf1c943276c18fcc7a9ca20b2b40cc049457c7',
-    host: 'ec2-23-23-173-30.compute-1.amazonaws.com',
-    database: 'dcmphg49j10vtj'
+    user: process.env.user,
+    password: process.env.password,
+    host: process.env.host,
+    database: process.env.database
 }
 
 const pool = new Pool(conopts);
