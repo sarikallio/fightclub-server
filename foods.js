a637e237cae847cb2f9c61b075918b19d385c50c
sarikallio <sari.kallio.sk@gmail.com>
2019-03-19 09:26:11 +0200

    serverin testaus

--- foods.js
+++ foods.js
@@ -1,27 +1,27 @@
 const Pool = require('pg').Pool;
-const conopts = {
-    user: 'postgres',
-    password: 'Sovelto1',
-    host: 'localhost',
-    database: 'fightclub'
-}
+// const conopts = {
+//     user: '',
+//     password: '',
+//     host: '',
+//     database: ''
+// }
 
-const pool = new Pool(conopts);
+//const pool = new Pool(conopts);
 
 const users = () => {
     return pool.connect()
-        .then(client => { 
-           const sql = 'SELECT name FROM foods';  
-            return client.query(sql)  
-                .then(res => {   
-                    client.release();  
-                    return res; 
-                })   
-                .catch(err => {   
-                    client.release();  
-                    console.error(err);  
-                });   
-        })   
+        .then(client => {
+           const sql = 'SELECT name FROM foods';
+            return client.query(sql)
+                .then(res => {
+                    client.release();
+                    return res;
+                })
+                .catch(err => {
+                    client.release();
+                    console.error(err);
+                });
+        })
    }
 
    module.exports = { users };
