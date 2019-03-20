7c86b285ac4c8e1886c2124df7d64985da4383b5
sarikallio <sari.kallio.sk@gmail.com>
2019-03-20 16:18:25 +0200

    emoji ensin

--- foods.js
+++ foods.js
@@ -12,7 +12,7 @@ const pool = new Pool(conopts);
 const foods = () => {
     return pool.connect()
         .then(client => {
-           const sql = 'SELECT name, emoji FROM foods';
+           const sql = 'SELECT emoji, name FROM foods';
             return client.query(sql)
                 .then(res => {
                     client.release();
