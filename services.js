25d36092d1fd08a3be554675ae0e8437b1bc9b95
sarikallio <sari.kallio.sk@gmail.com>
2019-03-25 14:55:48 +0200

    deletone toimii

--- services.js
+++ services.js
@@ -112,18 +112,6 @@ function deleteAll() {
     })
 }
 
-// function deleteOne(id) {
-//     return pool.connect()
-//     .then(client=>{
-//         let sql = "DELETE FROM feedback WHERE id=?";
-//         return client.query(sql, id)
-//         .then(resp=>{
-//             client.release();
-//             return resp;
-//         })
-//     })
-// }
-
 const deleteOne = (id) => {
     return pool.connect()
         .then(client => {
